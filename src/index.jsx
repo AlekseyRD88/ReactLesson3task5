589d92f6abd0b95c63aaa953f5d1f017b10ae02b
= <neymegen50@gmail.com>
2022-07-23 23:59:01 +0300

    init files

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './Profile.jsx';
+
+export const userData = {
+  firstName: 'James',
+  lastName: 'Bond',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London',
+};
+
+const rootElement = document.querySelector('#root');
+ReactDOM.render(<Profile {...userData}/>, rootElement);
