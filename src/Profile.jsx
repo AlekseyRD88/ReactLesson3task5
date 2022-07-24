0b2c95ff41a68854df9b5beae19c13ea15ab676a
= <neymegen50@gmail.com>
2022-07-24 12:00:13 +0300

    message

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -6,8 +6,8 @@ export const formatDate = date => moment(date).format("DD MMM YYYY");
 const Profile = (userData) => {
   return(
     <>
-    <div className="profile__name">{userData.firstName} {userData.lastName}</div>
-    <div className="profile__birth">Was born {formatDate(userData.birthDate)} in {userData.birthPlace}</div>
+    <div className="profile__name">{`${userData.firstName} ${userData.lastName}`}</div>
+    <div className="profile__birth">{`Was born ${formatDate(userData.birthDate)} in ${userData.birthPlace}`}</div>
     </>
   );
 };
